c83ceac866863b2f71a0f54c01254c3122c409cf
Burkard920 Burkard <stevenburkard24@yahoo.com>
2023-07-17 07:49:37 -0500

    social-feed-project updated to mvp

--- src/Components/PostList/PostList.jsx
+++ src/Components/PostList/PostList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Post from '../Post/Post';
 import './PostList.css';
 
+
 const PostList = ({ posts, onToggleLike }) => {
     return ( 
         <div className='postList'>
